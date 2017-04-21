acde9e0f47fd062463ccf1203bfeb4da788ec3e1
Anellla <anellla89@mail.ru>
2017-04-21 04:44:45 +0300

    testing branches

--- 2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$user = $_POST['login'];
-$password = $_POST['password'];
-header('Content-Type: application/json; charset=utf-8');
-$isValidUser = $user=='Lena' && $password=="1";
-echo $isValidUser? '{"error":null}': '{"error":"Incorrect user or password"}'; 
-?>
